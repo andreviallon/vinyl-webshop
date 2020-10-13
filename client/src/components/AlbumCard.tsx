d35142926c097b72b5fc3031f6c74be37a9c4329
andre_viallon <viallon.andre@gmail.com>
2020-10-13 11:48:02 +0200

    add images to album cards

--- client/src/components/AlbumCard.tsx
+++ client/src/components/AlbumCard.tsx
@@ -2,15 +2,29 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import { IAlbum } from '../mock/albums';
 
 interface Props {
   album: IAlbum;
 }
 
+const useStyles = makeStyles({
+  media: {
+    width: '100%',
+    objectFit: 'cover'
+  },
+});
+
 const AlbumCard: React.FC<Props> = ({ album }) => {
+    const classes = useStyles();
     return (
         <Card>
+            <img
+                className={classes.media}
+                src={album.image}
+                title="Album image"
+            />
             <CardContent>
                 <Typography variant="h6" component="h2">
                     {album.name}
--- client/src/components/LatestAlbums.tsx
+++ client/src/components/LatestAlbums.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
 import AlbumCard from './AlbumCard';
 import { albums, IAlbum } from '../mock/albums';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    center: {
+      [theme.breakpoints.down('sm')]: {
+        justifyContent: 'center'
+      }
+    }
+  })
+);
+
 const LatestAlbums = () => {
+    const classes = useStyles();
+
     return (
-        <Grid container spacing={3}>
+        <Grid container spacing={3} className={classes.center}>
             {albums.map((album: IAlbum) => (
                 <Grid item sm={12} md={6} lg={4} xl={3} key={album._id}>
                     <AlbumCard album={album} />
