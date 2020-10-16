dc68d92e15718853e46d15ba0cdf8edc69a747ec
andre_viallon <viallon.andre@gmail.com>
2020-10-16 13:03:26 +0200

    move album routes in its own file

---
+++ server/routes/albumRoutes.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import asyncHandler from 'expressAsyncHandler';
+import Album from '../models/albumModel.js';
+
+const router = express.Router();
+
+router.get('/', asyncHandler(async (req, res) => {
+    const albums = await Album.find({})
+    res.json(albums);
+}));
+
+router.get('/:id', asyncHandler(async (req, res) => {
+    const album = await Album.findById(req.params.id);
+
+    if (album) {
+        res.json(album);
+    } else {
+        res.status(404).json({ message: 'Album not found' });
+    }
+}));
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
-import albums from './data/albums.js';
+import albumRoutes from './routes/albumRoutes.js';
 
 dotenv.config();
 
@@ -14,14 +14,7 @@ app.get('/', (req, res) => {
     res.send('API is running');
 })
 
-app.get('/api/albums', (req, res) => {
-    res.json(albums);
-})
-
-app.get('/api/albums/:id', (req, res) => {
-    const album = albums.find(a => a._id === req.params.id);
-    res.json(album);
-})
+app.use('/api/albums', albumRoutes);
 
 const PORT = process.env.PORT || 5000
 
