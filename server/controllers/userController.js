d8b1e6cf2ccc9a3ed2bab70cfbf9925abb9dc0af
andre_viallon <viallon.andre@gmail.com>
2020-11-05 16:51:13 +0100

    add auth middleware

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -20,3 +20,19 @@ export const authUser = asyncHandler (async (req, res) => {
         throw new Error('Invalid email or password');
     }
 });
+
+export const getUserProfile = asyncHandler (async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        });
+    } else {
+        res.status(401);
+        throw new Error('User not found');
+    }
+});
